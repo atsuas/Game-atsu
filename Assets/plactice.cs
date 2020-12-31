40eb3ae3f75522cf4dd3e097d0884cb73eb0f506
as <>
2021-01-01 08:50:44 +0900

    メソッド・SayHelloの表示

---
+++ Assets/plactice.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plactice : MonoBehaviour
+{
+    void SayHello()
+    {
+        Debug.Log("Hello");
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SayHello();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
