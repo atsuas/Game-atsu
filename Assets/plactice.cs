41580d43092cb354e311ad213e07450822cff1ba
as <>
2021-01-01 09:30:44 +0900

    クラスの作成練習

--- Assets/plactice.cs
+++ Assets/plactice.cs
@@ -2,21 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Plactice : MonoBehaviour
+
+public class Player
 {
-    int Add(int a, int b)
+    private int hp = 100;
+    private int power = 50;
+
+    public void Attack()
     {
-        int c = a + b;
-        return c;
+        Debug.Log(this.power + "のダメージを与えた");
     }
 
+    public void Damage(int damage)
+    {
+        this.hp -= damage;
+        Debug.Log(damage + "のダメージを受けた");
+    }
+}
 
+public class Plactice : MonoBehaviour
+{
+ 
     // Start is called before the first frame update
     void Start()
     {
-        int answer;
-        answer = Add(2, 3);
-        Debug.Log(answer);
+        Player myPlayer = new Player();
+        myPlayer.Attack();
+        myPlayer.Damage(30);
     }
 
     // Update is called once per frame
