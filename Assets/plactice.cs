79fec92f2bd0e5abd3f78454d56ead5eebf00076
as <>
2021-01-01 10:08:13 +0900

    Vecterクラスの使い方

--- Assets/plactice.cs
+++ Assets/plactice.cs
@@ -2,33 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class Player
-{
-    private int hp = 100;
-    private int power = 50;
-
-    public void Attack()
-    {
-        Debug.Log(this.power + "のダメージを与えた");
-    }
-
-    public void Damage(int damage)
-    {
-        this.hp -= damage;
-        Debug.Log(damage + "のダメージを受けた");
-    }
-}
-
 public class Plactice : MonoBehaviour
 {
  
     // Start is called before the first frame update
     void Start()
     {
-        Player myPlayer = new Player();
-        myPlayer.Attack();
-        myPlayer.Damage(30);
+        Vector2 playerPos = new Vector2(3.0f, 4.0f);
+        playerPos.x += 8.0f;
+        playerPos.y += 5.0f;
+        Debug.Log(playerPos);
     }
 
     // Update is called once per frame
